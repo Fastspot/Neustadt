be6847b97621139391fbe1b9987a471b12ce6166
Fastspot <deathstar@fastspot.com>
2015-03-04 14:44:37 -0500

    Removed 'The ' when doing sorting -Tim

--- custom/inc/modules/creative-items.php
+++ custom/inc/modules/creative-items.php
@@ -25,7 +25,7 @@
 		
 		function getByCategories($cats){
 			$items = array();
-			$q = sqlquery("SELECT * FROM " . $this->Table . " WHERE archived = '' AND (type = " . implode(' OR type = ', $cats) . ") ORDER BY descriptive_title ASC");
+			$q = sqlquery("SELECT * FROM " . $this->Table . " WHERE archived = '' AND (type = " . implode(' OR type = ', $cats) . ") ORDER BY REPLACE(descriptive_title,'The ','') ASC");
 			while ($f = sqlfetch($q)) {
 				$items[] = $this->get($f);
 			}
